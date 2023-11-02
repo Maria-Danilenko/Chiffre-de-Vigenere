458f79b4a627cca55551a6adfb5a68ac27693892
Maria-Danilenko <73599645+Maria-Danilenko@users.noreply.github.com>
2023-11-02 19:50:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import time
-import os
-
-base_dir = os.path.dirname(__file__)
-
-message_file_path = os.path.join(base_dir, 'message.txt')
-encoded_file_path = os.path.join(base_dir, 'encodedMessage.txt')
-decoded_file_path = os.path.join(base_dir, 'decodedMessage.txt')
 
 ascii_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                  'u', 'v', 'w', 'x', 'y', 'z']
@@ -17,12 +10,11 @@ def encode(key, message):
         letM = message[i]
         letK = key[i % len(key)]
         encodLet = ascii_letters.index(letM) + ascii_letters.index(letK)
-        if encodLet > 25:
-            encodLet -= 26
+        if encodLet > 25: encodLet -= 26
         encodMess += ascii_letters[encodLet]
 
-    with open(encoded_file_path, 'w') as f:
-        f.write(encodMess)
+    print(encodMess)
+    return encodMess
 
 def decode(key, encodMess):
     decodMess = ''
@@ -31,30 +23,28 @@ def decode(key, encodMess):
         letM = encodMess[i]
         letK = key[i % len(key)]
         decodLet = ascii_letters.index(letM) - ascii_letters.index(letK)
-        if decodLet < 0:
-            decodLet += 26
+        if decodLet < 0: decodLet += 26
         decodMess += ascii_letters[decodLet]
 
-    with open(decoded_file_path, 'w') as f:
-        f.write(decodMess)
+    print(decodMess)
+
 
 if __name__ == '__main__':
-    with open(message_file_path) as f:
+    with open('message.txt') as f:
         message = f.read()
 
-    start = time.time()
-    message = message.lower().replace(' ', '').replace(',', '').replace('.', '').replace('-', '').replace('?', '').replace('?', '')
+    print(message)
 
-    encode('dog', message)
+    start = time.time()
+    message = message.lower().replace(' ','').replace(',','').replace('.','').replace('-','').replace('?','').replace('?','')
+
+    encodMess = encode('dog', message)
     end = time.time()
     runtime = end - start
     print(f"Час шифрування: {runtime} с")
 
-    with open(encoded_file_path) as f:
-        message = f.read()
-
     start = time.time()
-    decode('dog', message)
+    decode('dog', encodMess)
     end = time.time()
     runtime = end - start
     print(f"Час розшифрування: {runtime} с")
