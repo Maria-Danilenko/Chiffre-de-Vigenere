9a5e107b930237727b38ac7c66d3e76191d8bb6e
Maria-Danilenko <73599645+Maria-Danilenko@users.noreply.github.com>
2023-11-02 19:39:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import time
+import os
+
+base_dir = os.path.dirname(__file__)
+
+message_file_path = os.path.join(base_dir, 'message.txt')
+encoded_file_path = os.path.join(base_dir, 'encodedMessage.txt')
+decoded_file_path = os.path.join(base_dir, 'decodedMessage.txt')
 
 ascii_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                  'u', 'v', 'w', 'x', 'y', 'z']
@@ -10,10 +17,11 @@ def encode(key, message):
         letM = message[i]
         letK = key[i % len(key)]
         encodLet = ascii_letters.index(letM) + ascii_letters.index(letK)
-        if encodLet > 25: encodLet -= 26
+        if encodLet > 25:
+            encodLet -= 26
         encodMess += ascii_letters[encodLet]
 
-    with open('encodedMessage.txt', 'w') as f:
+    with open(encoded_file_path, 'w') as f:
         f.write(encodMess)
 
 def decode(key, encodMess):
@@ -23,26 +31,26 @@ def decode(key, encodMess):
         letM = encodMess[i]
         letK = key[i % len(key)]
         decodLet = ascii_letters.index(letM) - ascii_letters.index(letK)
-        if decodLet < 0: decodLet += 26
+        if decodLet < 0:
+            decodLet += 26
         decodMess += ascii_letters[decodLet]
 
-    with open('decodedMessage.txt', 'w') as f:
+    with open(decoded_file_path, 'w') as f:
         f.write(decodMess)
 
-
 if __name__ == '__main__':
-    with open('message.txt') as f:
+    with open(message_file_path) as f:
         message = f.read()
 
     start = time.time()
-    message = message.lower().replace(' ','').replace(',','').replace('.','').replace('-','').replace('?','').replace('?','')
+    message = message.lower().replace(' ', '').replace(',', '').replace('.', '').replace('-', '').replace('?', '').replace('?', '')
 
     encode('dog', message)
     end = time.time()
     runtime = end - start
     print(f"Час шифрування: {runtime} с")
 
-    with open('encodedMessage.txt') as f:
+    with open(encoded_file_path) as f:
         message = f.read()
 
     start = time.time()
@@ -50,10 +58,3 @@ if __name__ == '__main__':
     end = time.time()
     runtime = end - start
     print(f"Час розшифрування: {runtime} с")
-
-
-
-
-
-
-
